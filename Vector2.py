a34c7c115c1f2643c30aaca373f8600102633348
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2021-09-20 22:37:24 -0500

    Update Vector2.py

--- Vector2.py
+++ Vector2.py
@@ -4,28 +4,33 @@
 
 from math import sqrt
 
-__version__ = '0.0.3'
-NAME = 'Vector2 Module'
+__title__ = 'Vector2'
+__author__ = 'CoolCat467 & Will McGugan'
+__version__ = '0.1.0'
+__ver_major__ = 0
+__ver_minor__ = 1
+__ver_patch__ = 0
 
 class Vector2(object):
+    __slots__ 'x', 'y'
     def __init__(self, x=0, y=0):
-        if str(type(x)) in ("<class 'tuple'>", "<class 'list'>"):
+        if isinstance(x, (list, tuple)):
             x, y = x
         self.x = x
         self.y = y
     
     def __str__(self):
-        return "(%s, %s)" % (self.x, self.y)
+        return f'({self.x}, {self.y})'
     
     def __repr__(self):
         x, y = self.x, self.y
-        return "Vector2(%s, %s)" % (x, y)
+        return f'Vector2({self.x}, {self.y})'
     
     @staticmethod
     def from_points(frompoint, topoint):
         """Return a vector with the direction of frompoint to topoint"""
         P1, P2 = list(frompoint), list(topoint)
-        return Vector2(P2[0] - P1[0], P2[1] - P1[1])
+        return self.__class__(P2[0] - P1[0], P2[1] - P1[1])
     
     def get_magnitude(self):
         """Return the magnitude (length) of the vector"""
@@ -33,16 +38,7 @@ class Vector2(object):
     
     def get_distance_to(self, point):
         """Get the magnitude to a point"""
-##        px, py = list(point)
-##        sx, sy = list(self)
-##        try:
-##            px, py, sx, sy = float(px), float(py), float(sx), float(sy)
-##        except TypeError as e:
-##            print(type(point))
-##            print(list(point))
-##            raise TypeError(str(e))
-##        return Vector2(px - sx, py - sy).get_magnitude()
-        return Vector2.from_points(point, self).get_magnitude()
+        return self.__class__.from_points(point, self).get_magnitude()
     
     def normalize(self):
         """Normalize self (make into a unit vector)"""
@@ -53,7 +49,7 @@ class Vector2(object):
     
     def copy(self):
         """Make a copy of self"""
-        return Vector2(self.x, self.y)
+        return self.__class__(self.x, self.y)
     
     def __copy__(self):
         return self.copy()
@@ -66,39 +62,30 @@ class Vector2(object):
     
     #rhs is Right Hand Side
     def __add__(self, rhs):
-        return Vector2(self.x + rhs.x, self.y + rhs.y)
+        return self.__class__(self.x + rhs.x, self.y + rhs.y)
     
     def __sub__(self, rhs):
-        return Vector2(self.x - rhs.x, self.y - rhs.y)
+        return self.__class__(self.x - rhs.x, self.y - rhs.y)
     
     def __neg__(self):
-        return Vector2(-self.x, -self.y)
+        return self.__class__(-self.x, -self.y)
     
     def __mul__(self, scalar):
-        return Vector2(self.x * scalar, self.y * scalar)
+        return self.__class__(self.x * scalar, self.y * scalar)
     
     def __truediv__(self, scalar):
         try:
             x, y = self.x / scalar, self.y / scalar
         except ZeroDivisionError:
             x, y = self.x, self.y
-        return Vector2(x, y)
+        return self.__class__(x, y)
     
     def __len__(self):
         return 2
     
     def __iter__(self):
-        self.iterc = 0
-        return self
+        return iter([self.x, self.y])
     
-    def __next__(self):
-        if self.iterc <= 1:
-            val = [self.x, self.y][self.iterc]
-            self.iterc += 1
-            return val
-        else:
-            raise StopIteration
-    
-    def __getitem__(self, x):
-        return [self.x, self.y][x]
+    def __getitem__(self, idx):
+        return [self.x, self.y][idx]
     pass
