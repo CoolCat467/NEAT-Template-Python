153cb6ea9390c0522063bfbdcf33dc3872672213
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-01-06 16:56:18 -0600

    Add dmypy commands

---
+++ dmypy_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# Programmed by CoolCat467
+
+set -xe
+pip install -e ../NEAT-Template-Python
+dmypy --status-file="dmypy.json" run --timeout=1800 --log-file="log.txt" --export-types "src/neat/neat.py" -- --no-error-summary --warn-unused-ignores --show-error-codes --warn-unreachable --disallow-untyped-defs --soft-error-limit=-1 --warn-redundant-casts --disallow-untyped-calls --no-color-output --no-implicit-reexport --strict --show-absolute-path --cache-fine-grained --hide-error-context --cache-dir="/home/samuel/.idlerc/mypy" --show-traceback --show-column-numbers --show-error-end
+dmypy --status-file="dmypy.json" run --timeout=1800 --log-file="log.txt" --export-types "src/neat/neat.py" -- --no-error-summary --warn-unused-ignores --show-error-codes --warn-unreachable --disallow-untyped-defs --soft-error-limit=-1 --warn-redundant-casts --disallow-untyped-calls --no-color-output --no-implicit-reexport --strict --show-absolute-path --cache-fine-grained --hide-error-context --cache-dir="/home/samuel/.idlerc/mypy" --show-traceback --show-column-numbers --show-error-end
+dmypy --status-file="dmypy.json" run --timeout=1800 --log-file="log.txt" --export-types "examples/minimax/checkers_ai.py" -- --no-error-summary --warn-unused-ignores --show-error-codes --warn-unreachable --disallow-untyped-defs --soft-error-limit=-1 --warn-redundant-casts --disallow-untyped-calls --no-color-output --no-implicit-reexport --strict --show-absolute-path --cache-fine-grained --hide-error-context --cache-dir="/home/samuel/.idlerc/mypy" --show-traceback --show-column-numbers --show-error-end
